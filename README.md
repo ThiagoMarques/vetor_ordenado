09aacc672c9d218963523974bd4139bc518e589a
ThiagoMarques <thiagossmarques@gmail.com>
2023-04-19 09:11:46 -0300

    chore: insere código em python para ordenação de um vetor

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # vetor_ordenado
 Exemplo em Python para ordenação de um vetor
+
+- Facilita busca de elementos
+- O(n)
+- Pior caso = N
+
+----
+
+Exemplo:
+
+vetor.insere(6) 
+
+vetor.insere(4)
+
+vetor.insere(3)
+
+vetor.insere(5)
+
+vetor.insere(1)
+
+vetor.insere(8)
+
+vetor.imprime()
+
+-----
+
+INDEX - ELEM
+
+0  -  1
+
+1  -  3
+
+2  -  4
+
+3  -  5
+
+4  -  6
+
+5  -  8
---
+++ vetor_ordenado.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class VetorOrdenado:
+
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(self.capacidade, dtype=int)
+
+    # O(n)
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está vazio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, ' - ', self.valores[i])
+
+    # O(n)
+    def insere(self, valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            print('Capacidade máxima atingida')
+            return
+
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i] > valor:
+                break
+            if i == self.ultima_posicao:
+                posicao = i + 1
+
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+
+        self.valores[posicao] = valor
+        self.ultima_posicao += 1
+
+#vetor = VetorOrdenado(10)
+#vetor.imprime()
+
+#vetor.insere(6)
+#vetor.imprime()
